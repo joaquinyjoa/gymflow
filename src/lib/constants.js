9b6ef2db38f1795948d3f27cb923f7b135562e32
joaquinyjoa <joaquinalfredogreco@gmail.com>
2026-03-26 23:53:16 -0300

    Constantes de las rutas, dominio del email y roles

---
+++ src/lib/constants.js
@@ -0,0 +1,14 @@
+export const ROLES = {
+  ADMIN: 'admin',
+  ENTRENADOR: 'entrenador',
+  CLIENTE: 'cliente',
+}
+
+export const EMAIL_DOMAIN = '@retofitness.com'
+
+export const ROUTES = {
+  LOGIN: '/login',
+  ADMIN: '/admin',
+  ENTRENADOR: '/entrenador',
+  CLIENTE: '/rutina',
+}
